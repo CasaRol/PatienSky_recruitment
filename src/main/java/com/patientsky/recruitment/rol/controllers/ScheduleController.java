2e4d16b23d91f734e470a3f430e1f486ed8c2483
CasaRol <alexander-rol@hotmail.com>
2022-04-10 20:04:58 +0200

    First working version using 3 calendarID's

--- src/main/java/com/patientsky/recruitment/rol/controllers/ScheduleController.java
+++ src/main/java/com/patientsky/recruitment/rol/controllers/ScheduleController.java
@@ -1,12 +1,10 @@
 package com.patientsky.recruitment.rol.controllers;
 
-import com.patientsky.recruitment.rol.domain.Owner;
 import com.patientsky.recruitment.rol.schedules.CreateSchedulesOnStartUp;
 import com.patientsky.recruitment.rol.services.AppointmentPlanner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +18,15 @@ import java.util.UUID;
 public class ScheduleController {
 
     @Autowired
-    CreateSchedulesOnStartUp createSchedulesOnStartUp;
-
-    @Autowired
     AppointmentPlanner appointmentPlanner;
 
     @GetMapping("/findAvailableTime")
     public ResponseEntity<List> findAvailableTime() {
         List<UUID> testList = new ArrayList<>();
         testList.add(UUID.fromString("48cadf26-975e-11e5-b9c2-c8e0eb18c1e9"));
-        appointmentPlanner.findAvailableTime(testList, 10, "2019-03-01T13:00:00/2019-05-11T15:30:00");
+        testList.add(UUID.fromString("48644c7a-975e-11e5-a090-c8e0eb18c1e9"));
+        testList.add(UUID.fromString("452dccfc-975e-11e5-bfa5-c8e0eb18c1e9"));
+        appointmentPlanner.findAvailableTime(testList, 15, "2019-04-23T10:00:00/2019-04-23T18:45:00");
 
 
         //List<Owner> owners = createSchedulesOnStartUp.populateProvidedSchedules();
--- src/main/java/com/patientsky/recruitment/rol/services/AppointmentPlanner.java
+++ src/main/java/com/patientsky/recruitment/rol/services/AppointmentPlanner.java
@@ -1,13 +1,11 @@
 package com.patientsky.recruitment.rol.services;
 
-import com.patientsky.recruitment.rol.domain.Appointment;
 import com.patientsky.recruitment.rol.domain.Interval;
 import com.patientsky.recruitment.rol.domain.Owner;
 import com.patientsky.recruitment.rol.schedules.CreateSchedulesOnStartUp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -19,41 +17,138 @@ public class AppointmentPlanner {
     @Autowired
     CreateSchedulesOnStartUp createSchedulesOnStartUp = new CreateSchedulesOnStartUp(); //TODO: Fix BeanCreationException
 
-    private List<Owner> owners = createSchedulesOnStartUp.populateProvidedSchedules();
+    private final List<Owner> owners = createSchedulesOnStartUp.populateProvidedSchedules();
 
 
     public void findAvailableTime(List<UUID> calendarIds, int duration, String periodToSearch) {
 
-
         //Splitting timeInterval into start and end
         Map<String, String> intervals = startAndEndTimeInterval(periodToSearch);
 
+        //Interval for Needed free time lookup
         Date startOfInterval = new Date();
         Date endOfInterval = new Date();
 
         try {
             startOfInterval = sdf.parse(intervals.get("start"));
             endOfInterval = sdf.parse(intervals.get("end"));
-        } catch (ParseException p) {
+        } catch (Exception p) {
             p.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
-        Owner owner1 = null;
+        List<Owner> affectedOwners = new ArrayList<>();
 
+        //Getting the owner objects for each calendarId given as parameter
         for (Owner owner : owners) {
-            if(owner.getAppointments().get(0).getCalendarId().equals(calendarIds.get(0))) {
-                owner1 = owner;
+            for (UUID calendarId : calendarIds) {
+                if (owner.getAppointments().get(0).getCalendarId().equals(calendarId)) {
+                    affectedOwners.add(owner); //isolating only owners who is a part of this scheduling
+                }
             }
         }
 
-        List<Interval> tmpList = calcOwnerFreeTimeInSchedule(owner1);
-        for(Interval interval: tmpList) {
-            System.out.println(interval);
+        List<List<Interval>> ownerFreeTime = new ArrayList<>();
+
+        //Adding each owners free time to the map
+        for (Owner owner : affectedOwners) {
+            List<Interval> tmpList = calcOwnerFreeTimeInSchedule(owner, duration, startOfInterval, endOfInterval);
+            for(Interval interval : tmpList) {
+                System.out.println(owner.getName() + ": " + interval);
+            }
+            ownerFreeTime.add(tmpList);
+
         }
 
+        List<Interval> foundMatches = findCommonAvailableTimes(ownerFreeTime, duration);
 
+
+    }
+
+    private List<Interval> findCommonAvailableTimes(List<List<Interval>> possibleIntervals, int duration) {
+        System.out.println("Looking for matches");
+        List<Interval> matchList = possibleIntervals.get(0); //Instantiating first index as possible solutions as a starting point
+
+        if (possibleIntervals.size() > 1) { //Early return in only one calenderID is provided
+
+            for (int i = 1; i < possibleIntervals.size(); i++) { //Skipping first Owner as it has already been set as a complete match
+
+                //Creating a temporary List in which to enter new proposed times due to overlap (ConcurrentModificationException handling)
+                List<Interval> updatedOptions = new ArrayList<>();
+
+                for (Interval ownerFreeTimeOptions : possibleIntervals.get(i)) {
+                    for (Interval currentMatch : matchList) {
+
+                        //ownerFreeTimeOptions is within match
+                        if ((ownerFreeTimeOptions.getStart().equals(currentMatch.getStart()) ||
+                                ownerFreeTimeOptions.getStart().toInstant().isAfter(currentMatch.getStart().toInstant())) &&
+                                (ownerFreeTimeOptions.getEnd().equals(currentMatch.getEnd()) ||
+                                        ownerFreeTimeOptions.getEnd().toInstant().isBefore(currentMatch.getEnd().toInstant()))) {
+
+                            //Match only fits within currentMatch... replacing intervals
+                            updatedOptions.add(ownerFreeTimeOptions);
+                        }
+                        //ownerFreeTime completely overlaps currentMatch
+                        else if ((ownerFreeTimeOptions.getStart().equals(currentMatch.getStart()) ||
+                                ownerFreeTimeOptions.getStart().toInstant().isBefore(currentMatch.getStart().toInstant())) &&
+                                (ownerFreeTimeOptions.getEnd().equals(currentMatch.getEnd()) ||
+                                        ownerFreeTimeOptions.getEnd().toInstant().isAfter(currentMatch.getEnd().toInstant()))) {
+                            //Positive match - Keeping currentMatch and moving on to the next possibility
+                            updatedOptions.add(currentMatch);
+                        }
+                        //Checking overlap of first half
+                        else if (ownerFreeTimeOptions.getStart().equals(currentMatch.getStart()) ||
+                                ownerFreeTimeOptions.getStart().toInstant().isBefore(currentMatch.getStart().toInstant()) &&
+                                        ownerFreeTimeOptions.getEnd().toInstant().isBefore(currentMatch.getEnd().toInstant())) {
+                            //Checking if overlap allows sufficient time
+                            if (calcTimeBetweenTwoPoints(ownerFreeTimeOptions.getStart(), currentMatch.getEnd()) < duration) {
+                                //Not a good match - skip adding for next iteration
+                                continue;
+                            } else {
+                                //Creating compromised time due to overlap
+                                Interval newTime = new Interval();
+                                newTime.setStart(currentMatch.getStart());
+                                newTime.setEnd(ownerFreeTimeOptions.getEnd());
+                                newTime.setIntervalMinutes(calcTimeBetweenTwoPoints(newTime.getStart(), newTime.getEnd()));
+
+                                //Adding to the list for next iteration
+                                updatedOptions.add(newTime);
+                            }
+
+                        }
+                        //Checking overlap of second half
+                        else if (ownerFreeTimeOptions.getStart().toInstant().isAfter(currentMatch.getStart().toInstant()) &&
+                                (ownerFreeTimeOptions.getEnd().equals(currentMatch.getEnd()) ||
+                                        ownerFreeTimeOptions.getEnd().toInstant().isAfter(currentMatch.getEnd().toInstant()))) {
+                            //Checking if overlap allows sufficient time
+                            if (calcTimeBetweenTwoPoints(currentMatch.getStart(), ownerFreeTimeOptions.getStart()) < duration) {
+                                //Not a good match - Removing from matches
+                                continue;
+                            } else {
+                                //Creating compromised time due to overlap
+                                Interval newTime = new Interval();
+                                newTime.setStart(ownerFreeTimeOptions.getStart());
+                                newTime.setEnd(currentMatch.getEnd());
+                                newTime.setIntervalMinutes(calcTimeBetweenTwoPoints(newTime.getStart(), newTime.getEnd()));
+
+                                //Adding to list for next iteration
+                                updatedOptions.add(newTime);
+                            }
+                        }
+                    }
+
+                }
+                //Setting matchList to the new proposed times
+                matchList = updatedOptions;
+            }
+
+            matchList.removeIf(interval -> interval.getIntervalMinutes() <= 0);
+
+            for (Interval matches : matchList) {
+                System.out.println("Common time: " + matches);
+            }
+
+        }
+        return matchList;
     }
 
     //Splits out the timeInterval provided from REST into Start and End
@@ -68,27 +163,54 @@ public class AppointmentPlanner {
         return startAndEnd;
     }
 
-    private List<Interval> calcOwnerFreeTimeInSchedule(Owner owner) {
+    private List<Interval> calcOwnerFreeTimeInSchedule(Owner owner, int duration, Date startInterval, Date endInterval) {
         List<Interval> freeSlots = new ArrayList<>();
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < owner.getAppointments().size() - 1; i++) {
             Interval freetime = new Interval();
 
             //Setting start and end time to open spot in schedule
-            freetime.setStart(owner.getAppointments().get(i).getEnd());
-            freetime.setEnd(owner.getAppointments().get(i+1).getStart());
+            Date start = owner.getAppointments().get(i).getStart();
+            Date end = owner.getAppointments().get(i).getEnd();
 
-            //Calculating interval duration
-            long duration = TimeUnit.MILLISECONDS.toMinutes(freetime.getEnd().getTime() - freetime.getStart().getTime());
+            //Evaluating if open space between appointments are withing given interval
+            if (owner.getAppointments().get(i).getEnd().toInstant().isAfter(startInterval.toInstant()) &&
+                    (owner.getAppointments().get(i + 1).getStart().toInstant().isBefore(endInterval.toInstant()) ||
+                            owner.getAppointments().get(i).getEnd().equals(endInterval))) {
 
-            int durationInMinutes = Math.toIntExact(duration);
+                //Setting start and end of freeTime
+                freetime.setStart(owner.getAppointments().get(i).getEnd());
+                freetime.setEnd(owner.getAppointments().get(i + 1).getStart());
 
-            //Setting interval to open spot in minutes
-            freetime.setIntervalMinutes(durationInMinutes);
+                //Making sure free time is NOT outside specified start and end intervals of the search
+                if(freetime.getStart().toInstant().isBefore(startInterval.toInstant())) {
+                    freetime.setStart(startInterval);
+                } else if(freetime.getEnd().toInstant().isAfter(endInterval.toInstant())) {
+                    freetime.setEnd(endInterval);
+                }
 
-            freeSlots.add(freetime);
+                //Calculating time between start and end
+                int durationInMinutes = calcTimeBetweenTwoPoints(freetime.getStart(), freetime.getEnd());
+
+                //Setting interval to open spot in minutes
+                freetime.setIntervalMinutes(durationInMinutes);
+
+                //only adding freeTime if it fits within the freeTimeDuration of needed appointment
+                if (freetime.getIntervalMinutes() >= duration) {
+                    freeSlots.add(freetime);
+                }
+            }
+
         }
+
         return freeSlots;
+    }
+
+    private int calcTimeBetweenTwoPoints(Date start, Date end) {
+        //Calculating interval freeTimeDuration
+        long freeTimeDuration = TimeUnit.MILLISECONDS.toMinutes(end.getTime() - start.getTime());
+
+        return Math.toIntExact(freeTimeDuration);
     }
 
 }
